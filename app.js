41d5c1092db2e91427914a9436e5d5d4411ea00b
Jassem Al-Buloushi <jassem@MACs-MacBook-Pro-2.local>
2018-10-17 17:55:10 +0300

    compare process.args vs. yargs

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ const yargs = require("yargs");
 
 const notes = require("./notes.js");
 
+const argv = yargs.argv; //yargs own argv argumments
 var command = process.argv[2];
-
 console.log("Command: ", command);
-console.log(process.argv);
+console.log("Process: ", process.argv);
+console.log("Yargs: ", argv);
 
 if(command === "add") {
   console.log("Adding new note");
