a9d58272b7db6a33ee78eb4483f5454ca52dbcb0
Jassem Al-Buloushi <jassem@MACs-MacBook-Pro-2.local>
2018-10-17 11:11:27 +0300

    export add fn

--- notes.js
+++ notes.js
@@ -1,5 +1,10 @@
 console.log("Strating notes.js");
 
+module.exports.addNote = () => {
+  console.log("addNote");
+  return "New Note";
+};
+
 module.exports.add = (a, b) => {
   var sum = a + b;
   return sum;
