ac53a2602c423fc50fa2abd93e1ecfb883f64ff6
Jassem Al-Buloushi <jassem@MACs-MacBook-Pro-2.local>
2018-10-17 17:31:52 +0300

    make if statements for command

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ const _ = require("lodash");
 
 const notes = require("./notes.js");
 
-console.log(process.argv);
+var command = process.argv[2];
+
+console.log("Command: ", command);
+
+if(command === "add") {
+  console.log("Adding new note");
+} else if(command === "list") {
+  console.log("Listing all notes");
+} else {
+  console.log("Command not recognized");
+}
