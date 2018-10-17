ded0bbab7c9d01d0f4cc4f29e4c85bca59a4f581
Jassem Al-Buloushi <jassem@MACs-MacBook-Pro-2.local>
2018-10-17 10:33:20 +0300

    create var user

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ console.log("Starting app");
 const fs = require("fs");
 const os = require("os");
 
+var user = os.userInfo();
+
 fs.appendFile("greetings.txt", "Hello World!!", function (err) {
   if(err) {
     console.log("Unable to write to file");
