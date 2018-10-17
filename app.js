10219a6e225a6478fc6d86d35ff3edd603e339a9
Jassem Al-Buloushi <jassem@MACs-MacBook-Pro-2.local>
2018-10-17 18:22:17 +0300

    add getAll, getNote, removeNote methods

--- app.js
+++ app.js
@@ -9,17 +9,16 @@ const notes = require("./notes.js");
 const argv = yargs.argv; //yargs own argv argumments
 var command = process.argv[2];
 console.log("Command: ", command);
-console.log("Process: ", process.argv);
 console.log("Yargs: ", argv);
 
 if(command === "add") {
   notes.addNote(argv.title, argv.body);
 } else if(command === "list") {
-  console.log("Listing all notes");
+  notes.getAll();
 } else if(command === "read") {
-  console.log("Reading note");
+  notes.getNote(argv.title);
 } else if(command === "remove") {
-  console.log("Deleting note");
+  notes.removeNote(argv.title);
 } else {
   console.log("Command not recognized");
 }
--- notes.js
+++ notes.js
@@ -4,6 +4,22 @@ var addNote = (title, body) => {
   console.log("Adding note", title, body);
 };
 
+var getAll = () => {
+  console.log("Getting all notes");
+};
+
+var getNote = (title) => {
+  console.log("Getting note", title);
+};
+
+var removeNote = (title) => {
+  console.log("Remove note", title);
+};
+
+// ES5 addNote: addNote, ES6 addNote if they are have the same name
 module.exports = {
-  addNote
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
