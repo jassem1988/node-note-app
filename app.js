ca969be50b9d7cd2b498a29f0264c37fd2f6ecd0
Jassem Al-Buloushi <jassem@MACs-MacBook-Pro-2.local>
2018-10-19 16:48:35 +0300

    remove console.log 's

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log("Starting app.js");
-
 const fs = require("fs");
 const _ = require("lodash");
 const yargs = require("yargs");
@@ -8,8 +6,6 @@ const notes = require("./notes.js");
 
 const argv = yargs.argv; //yargs own argv argumments
 var command = argv._[0];
-console.log("Command: ", command);
-console.log("Yargs: ", argv);
 
 if(command === "add") {
   var note = notes.addNote(argv.title, argv.body);
--- notes.js
+++ notes.js
@@ -1,5 +1,3 @@
-console.log("Strating notes.js");
-
 const fs = require("fs");
 
 var fetchNotes = () => {
