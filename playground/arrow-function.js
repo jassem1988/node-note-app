d13e3b795dce1b388d4513e55c3f490d6f785be4
Jassem Al-Buloushi <jassem@MACs-MacBook-Pro-2.local>
2018-10-21 13:09:07 +0300

    practice arrow fn's

--- playground/arrow-function.js
+++ playground/arrow-function.js
@@ -1,6 +1,17 @@
-var square = (x) => {
-  var result = x * x;
-  return result;
+var square = x => x * x;
+console.log(square(9));
+
+var user = {
+  name: 'Jassem',
+  sayHi: () => {
+    console.log(arguments);
+    console.log(`Hi I'm ${this.name}`);
+  },
+  sayHiAlt () {
+    console.log(arguments);
+    console.log(`Hi I'm ${this.name}`);
+  }
 };
 
-console.log(square(3));
+// user.sayHi(1, 2, 3);
+user.sayHiAlt(1, 2, 3);
